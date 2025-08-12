c71116b2a32f995b3d85536993a165ec831b5b19
natanaeldev20 <natanaelvasquez150103@gmail.com>
2025-08-12 03:45:28 -0500

    coraline

--- .vercel/output/_functions/chunks/Container_Dejq6jGm.mjs
+++
@@ -1,9 +0,0 @@
-import { c as createComponent, m as maybeRenderHead, d as renderSlot, a as renderTemplate } from './astro/server_muSZyNsG.mjs';
-import 'kleur/colors';
-import 'clsx';
-
-const $$Container = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`${maybeRenderHead()}<div class="max-w-6xl m-auto"> ${renderSlot($$result, $$slots["default"])} </div>`;
-}, "C:/Users/natan/Escritorio/project/institucioneducativa89007/src/components/ui/Container.astro", void 0);
-
-export { $$Container as $ };
