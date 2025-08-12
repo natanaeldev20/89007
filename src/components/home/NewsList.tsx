1f9a819be887299f90997f9f82904514aced7796
natanaeldev20 <natanaelvasquez150103@gmail.com>
2025-08-12 18:00:00 -0500

    esoesoesoesoeso

--- src/components/home/NewsList.tsx
+++ src/components/home/NewsList.tsx
@@ -1,26 +1,47 @@
-// import { useEffect, useState } from "react";
-// import NewsCard from "@components/home/NewsCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-// interface News {
-//   id: string;
-//   imageUrl: string;
-//   title: string;
-// }
+interface News {
+  id: string;
+  imageUrl: string;
+  title: string;
+}
 
-// export default function NewsList() {
-//   const [news, setNews] = useState<News[]>([]);
+const NewsList = () => {
+  const [news, setNews] = useState<News[]>([]);
 
-//   useEffect(() => {
-//     fetch("https://cpanel89007.vercel.app/api/school-news/latest3")
-//       .then((res) => res.json())
-//       .then((data) => setNews(data));
-//   }, []);
+  useEffect(() => {
+    // fetch("https://cpanel89007.vercel.app/api/school-news/latest3")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log("API response:", data);
+    //     setNews(data.news);
+    //   });
+    const getNews = async () => {
+      const { data } = await axios.get(
+        "https://cpanel89007.vercel.app/api/school-news/latest3"
+      );
+      setNews(data.news);
+    };
 
-//   return (
-//     <>
-//       {news.map((item) => (
-//         <NewsCard key={item.id} url={item.imageUrl} title={item.title} />
-//       ))}
-//     </>
-//   );
-// }
+    getNews();
+  }, []);
+
+  return (
+    <>
+      <div className="w-full grid grid-cols-3">
+        {news.map(({ id, imageUrl, title }) => (
+          <div key={id}>
+            <h1>{title}</h1>
+            <img src={imageUrl} alt={title} />
+          </div>
+        ))}
+      </div>
+      <div>
+        <h1>ASTROOOOO</h1>
+      </div>
+    </>
+  );
+};
+
+export default NewsList;
