84b265997f8157f0a2daf7f5fdb93ed1909ab7bb
natanaeldev20 <natanaelvasquez150103@gmail.com>
2025-08-12 14:17:13 -0500

    production

--- src/components/home/NewsList.tsx
+++ src/components/home/NewsList.tsx
@@ -1,14 +1,26 @@
-import NewsCard from "@components/home/NewsCard";
-import { getNews3 } from "@services/newsService";
+// import { useEffect, useState } from "react";
+// import NewsCard from "@components/home/NewsCard";
 
-const data = await getNews3();
+// interface News {
+//   id: string;
+//   imageUrl: string;
+//   title: string;
+// }
 
-export default function NewsList() {
-  return (
-    <>
-      {data.map((news) => (
-        <NewsCard key={news.id} url={news.imageUrl} title={news.title} />
-      ))}
-    </>
-  );
-}
+// export default function NewsList() {
+//   const [news, setNews] = useState<News[]>([]);
+
+//   useEffect(() => {
+//     fetch("https://cpanel89007.vercel.app/api/school-news/latest3")
+//       .then((res) => res.json())
+//       .then((data) => setNews(data));
+//   }, []);
+
+//   return (
+//     <>
+//       {news.map((item) => (
+//         <NewsCard key={item.id} url={item.imageUrl} title={item.title} />
+//       ))}
+//     </>
+//   );
+// }
