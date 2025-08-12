57b14ffed38bb54bab59cf171e56b56d25655b05
natanaeldev20 <natanaelvasquez150103@gmail.com>
2025-08-12 14:42:28 -0500

    jajajaja

--- src/components/home/NewsList.tsx
+++ src/components/home/NewsList.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import NewsCard from "@components/home/NewsCard";
+// import { useEffect, useState } from "react";
+// import NewsCard from "@components/home/NewsCard";
 
-interface News {
-  id: string;
-  imageUrl: string;
-  title: string;
-}
+// interface News {
+//   id: string;
+//   imageUrl: string;
+//   title: string;
+// }
 
-export default function NewsList() {
-  const [news, setNews] = useState<News[]>([]);
+// export default function NewsList() {
+//   const [news, setNews] = useState<News[]>([]);
 
-  useEffect(() => {
-    fetch("https://cpanel89007.vercel.app/api/school-news/latest3")
-      .then((res) => res.json())
-      .then((data) => setNews(data));
-  }, []);
+//   useEffect(() => {
+//     fetch("https://cpanel89007.vercel.app/api/school-news/latest3")
+//       .then((res) => res.json())
+//       .then((data) => setNews(data));
+//   }, []);
 
-  return (
-    <>
-      {news.map((item) => (
-        <NewsCard key={item.id} url={item.imageUrl} title={item.title} />
-      ))}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {news.map((item) => (
+//         <NewsCard key={item.id} url={item.imageUrl} title={item.title} />
+//       ))}
+//     </>
+//   );
+// }
